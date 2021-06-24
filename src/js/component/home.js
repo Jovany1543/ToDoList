5d609c53ac23df92b09a8f52bf4117af87867dd6
Jovany1543 <christian.marrero3@gmail.com>
2021-06-24 20:20:53 +0000

    creating repo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export function ToDoList() {
+	const [myArr, setMyArr] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const handleKeyPress = e => {
+		if (e.key === "Enter" && inputValue !== "") {
+			setMyArr(
+				myArr.concat({
+					label: inputValue,
+					done: false
+				})
+			);
+			setInputValue("");
+		}
+	};
 
-//create your first component
-export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>todos</h1>
+			<ul className="todo-wrap list-unstyled">
+				<li>
+					<input
+						type="text"
+						onChange={e => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyPress={e => handleKeyPress(e)}
+					/>
+				</li>
+
+				{myArr.map((item, index) => (
+					<li key={index}>{item.label}</li>
+				))}
+				<li>
+					{myArr.length > 0
+						? `${myArr.length} Items`
+						: "No tasks, add a task"}
+
+					<span
+						role="button"
+						tabIndex="0"
+						onClick={() => setMyArr([])}>
+						Clear list
+					</span>
+				</li>
+			</ul>
 		</div>
 	);
 }
